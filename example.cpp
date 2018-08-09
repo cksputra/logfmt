15eed380ba89ef44fc955cb2bd6c5fdc226b69de
cks <ogatatsumi97@yahoo.com>
2018-08-09 17:46:25 +0700

    change function format

--- example.cpp
+++ example.cpp
@@ -13,19 +13,19 @@ int main()
   logfmt log("test", toFile);
 
   spdlog::set_level(spdlog::level::trace);
-  log.WithField(log.custom_field={{"animal", "walrus"}, {"total", 100}}).info("INFO of WALRUS and its total");
+  log.with_field(log.custom_field={{"animal", "walrus"}, {"total", 100}}).info("INFO of WALRUS and its total");
   log.info("No Walrus");
-  log.WithField(log.custom_field={{"animal", "dog"}, {"total", float_test}}).warn("WARN of DOG and its total");
+  log.with_field(log.custom_field={{"animal", "dog"}, {"total", float_test}}).warn("WARN of DOG and its total");
   log.warn("No Dog");
 
   
-  log.OutputJSON(); //output in JSON format
-  log.WithField(log.custom_field={{"animal", "cat"}, {"total", long_test}}).critical("CRITICAL of CAT and its total");
+  log.JSON_output(); //output in JSON format
+  log.with_field(log.custom_field={{"animal", "cat"}, {"total", long_test}}).critical("CRITICAL of CAT and its total");
   log.critical("No Cat");
-  log.WithField(log.custom_field={{"animal", "falcon"}, {"total", 10}}).error("ERROR of FALCON and its total");
+  log.with_field(log.custom_field={{"animal", "falcon"}, {"total", 10}}).error("ERROR of FALCON and its total");
   log.error("No Falcon");
 
-  auto staticformat = log.WithField(log.custom_field={{"float",float_test}, {"long", long_test}, {"string", a}});
+  auto staticformat = log.with_field(log.custom_field={{"float",float_test}, {"long", long_test}, {"string", a}});
   staticformat.info("This is INFO with custom key");
   staticformat.debug("This is DEBUG with custom key");
   staticformat.trace("This is TRACE with custom key");
@@ -33,8 +33,8 @@ int main()
   staticformat.critical("This is CRITICAL with custom key");
   staticformat.error("This is ERROR with custom key");
 
-  log.OutputDefault();
-  auto staticformat2 = log.WithField(log.custom_field={{"animal","griffin"}, {"rider", "azure"}, {"weapon", "spear"}});
+  log.default_output();
+  auto staticformat2 = log.with_field(log.custom_field={{"animal","griffin"}, {"rider", "azure"}, {"weapon", "spear"}});
   staticformat2.info("This is INFO with custom key");
   staticformat2.debug("This is DEBUG with custom key");
   staticformat2.trace("This is TRACE with custom key");
--- logfmt.h
+++ logfmt.h
@@ -20,13 +20,14 @@ private:
       std::shared_ptr<spdlog::logger> entry_log;
       bool JSON;
       //function
-      std::string AddQuotation(std::string msg);
-      void processKey(std::string& msg);
-      std::string casting(std::string key, std::any val);
+      std::string add_quotation(std::string msg); 
+      void process_key(std::string& msg);
+      std::string cast_default(std::string key, std::any val);
+      std::string cast_JSON(std::string key, std::any val);
     public:
       Entry(); //default ctor
       Entry(std::shared_ptr<spdlog::logger>& log, bool JSON ); //ctor with parameter
-      Entry& WithField(std::map<std::string, std::any>& keyMap);
+      Entry& with_field(std::map<std::string, std::any>& keyMap);
       void info(std::string msg);
       void trace(std::string msg);
       void debug(std::string msg);
@@ -34,22 +35,23 @@ private:
       void error(std::string msg);
       void critical(std::string msg);
   };
-  Entry loggerEntry;
+  Entry logger_entry;
   std::shared_ptr<spdlog::logger> logger;
-  bool JSONstatus; // make this true to make log in JSON form / false in normal form
-  bool WriteFile; // make this true to write log into file
+  bool JSON_status; // make this true to make log in JSON form / false in normal form
+  bool write_file; // make this true to write log into file
+  //private function
+  std::string JSON_formatter(); // make the log in JSON format
+  std::string add_quotation(std::string msg);
 public:
   //member
   std::map<std::string, std::any> custom_field;
   //ctor
   logfmt(std::string name, bool to_file);
   //function
-  void OutputJSON();
-  void OutputDefault();
-  logfmt::Entry& WithField(std::map<std::string, std::any>& keyMap);
-  std::string JSONformatter();
-  std::string AddQuotation(std::string msg);
-
+  void JSON_output();
+  void default_output();
+  logfmt::Entry& with_field(std::map<std::string, std::any>& keyMap);
+  
   //basic logging
   void info(std::string msg);
   void trace(std::string msg);
